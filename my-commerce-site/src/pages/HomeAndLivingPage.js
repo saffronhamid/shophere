dc7c613ebf89318dcdb55f0add096e4dddb77589
saffronhamid <saffronhamid1234@gmail.com>
2024-10-20 22:06:13 +0200

    Updated HomeAndLivingPage

--- my-commerce-site/src/pages/HomeAndLivingPage.js
+++ my-commerce-site/src/pages/HomeAndLivingPage.js
@@ -1,35 +1,25 @@
 import React from 'react';
-import './HomeAndLivingPage.css'; // Create CSS file for HomeLivingPage-specific styles
+import './HomeAndLivingPage.css'; // Assuming you have a separate CSS file
 
-const HomeLivingPage = () => {
-  return (
-    <div className="home-living-container">
-      <div className="home-section interior">
-        <h2>Interior</h2>
-        <img
-          src="https://plus.unsplash.com/premium_photo-1678447489180-34bace05ab6e?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTN8fGhvbWUlMjBkZWNvciUyMGludGVyaW9yfGVufDB8fDB8fHww"
-          alt="Interior"
-          className="home-image"
-        />
-      </div>
-      <div className="home-section exterior">
-        <h2>Exterior</h2>
-        <img
-          src="https://media.istockphoto.com/id/1677035251/photo/modern-luxurious-villa-balcony-with-green-garden-in-pine-forest.webp?a=1&b=1&s=612x612&w=0&k=20&c=Tc1FUlKkdNsKXPno7ioeqP2vMDj_k8FSfA3hPFge9Ls="
-          alt="Exterior"
-          className="home-image"
-        />
-      </div>
-      <div className="home-section decor">
-        <h2>Decor</h2>
-        <img
-          src="https://plus.unsplash.com/premium_photo-1669385170571-43397a00fcbe?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8aG9tZSUyMGRlY29yJTIwY2hyaXN0bWFzfGVufDB8fDB8fHww"
-          alt="Decor"
-          className="home-image"
-        />
-      </div>
-    </div>
-  );
+const HomeAndLivingPage = () => {
+    return (
+        <div className="categories-section">
+            <div className="category-card">
+                <img src="https://plus.unsplash.com/premium_photo-1661778773089-8718bcedb39e?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8aG9tZSUyMGludGVyaW9yfGVufDB8fDB8fHww" alt="Interior" />
+                <h3>Interior</h3>
+            </div>
+            <div className="category-card">
+                <img src="https://plus.unsplash.com/premium_photo-1661908377130-772731de98f6?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8aG9tZSUyMGV4dGVyaW9yfGVufDB8fDB8fHww" alt="Exterior" />
+                <h3>Exterior</h3>
+            </div>
+            <div className="category-card">
+                <img src="https://media.istockphoto.com/id/1685138093/photo/happy-woman-moving-house-and-unrolling-a-carpet-while-unpacking.webp?a=1&b=1&s=612x612&w=0&k=20&c=1XZlbFmkcb2pdpA4fnvJsM_B7-ufYH_daTc_sqFtaY0=" alt="Decor" />
+                <h3>Decor</h3>
+            </div>
+            
+        </div>
+        
+    );
 };
 
-export default HomeLivingPage;
+export default HomeAndLivingPage;
